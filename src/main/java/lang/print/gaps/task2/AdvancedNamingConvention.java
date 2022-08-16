05ea70b60503cd07077fc4e15bec6bdb913f8687
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-16 22:39:15 +0500

    Update and rename advancedNamingConvention.java to AdvancedNamingConvention.java

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,16 +1,15 @@
 package lang.print.gaps.task2;
-
 public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
 
-    void сallToFriend() {
+    void callToFriend() {
         //some code
     }
 
-    void сallByNumber(int number) {
+    void callByNumber(int Number) {
         //some code
     }
 }
